b9d976d0304d97028c1cda4011d15b14158ae16c
sotatek <sotatek@MacBook-Pro-cua-sotatek-3.local>
2022-09-29 11:38:22 +0700

    add head tag

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,14 +1,24 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import React from "react";
+import Head from "next/head";
 
 const Layout = ({ children }: { children: React.ReactElement }) => {
   return (
-    <div className="m-auto flex h-full max-w-5xl flex-col">
-      <Header />
-      <div className="flex-grow  py-10">{children}</div>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1103570262283389"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
+      <div className="m-auto flex h-full max-w-5xl flex-col">
+        <Header />
+        <div className="flex-grow  py-10">{children}</div>
+        <Footer />
+      </div>
+    </>
   );
 };
 
